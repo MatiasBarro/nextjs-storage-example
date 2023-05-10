991ad5a49a87de35b55e91d0b506da6d6e348441
Matias Barro <matias.barro@gmail.com>
2023-05-10 18:42:54 -0300

    First Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+// import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1 style={{marginBottom: '3rem'}}>NextJs Storage Example</h1>
+        {children}
+      </body>
     </html>
   )
 }
